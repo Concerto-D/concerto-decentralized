6210616b548ad8d56a8b021365d1560a3e17f83f
hcoullon <helene.coullon@inria.fr>
2018-08-22 18:07:21 +0200

    fix deploy_par_up.py

--- examples/scalability/deploy_par_up.py
+++ examples/scalability/deploy_par_up.py
@@ -11,28 +11,31 @@ if __name__ == '__main__':
         print("deploy_par_up.py <number of user components> <number of "
               "transitions inside user components>\n")
         sys.exit(-1)
+    else:
+        nbcomp = int(sys.argv[1])
+        nbtrans = int(sys.argv[2])
+        if nbcomp < 1:
+            print("*** Warning: at least 1 user component is deployed by "
+                  "this example. 1 component will be deployed.\n")
+            nbcomp = 1
+        if nbtrans < 1:
+            print("*** Warning: at least 1 transition is needed inside the "
+                  "user components. 1 transition will be deployed.\n")
+            nbtrans = 1
 
-    # Composant Provider
-    provider = Provider()
-    ass = Assembly()
-    ass.addComponent('provider', provider)
+        # Composant Provider
+        provider = Provider()
+        ass = Assembly()
+        ass.addComponent('provider', provider)
 
-    nbcomp = sys.argv[1]
-    nbtrans = sys.argv[2]
+        users = []
 
-    # first user
-    u0 = UserNTrans()
-    u0.createTransitions(nbtrans)
-    ass.addComponent('u0', u0)
+        for i in range(0, nbcomp):
+            users.append(UserNTrans())
+            users[i].createTransitions(nbtrans)
+            name = "u" + str(i)
+            ass.addComponent(name, users[i])
+            ass.addConnection(provider, 'service', users[i], 'service')
 
-    ass.addConnection(provider, 'service', u0, 'service')
-
-    for i in range(1, int(nbcomp)):
-        exec("u" + str(i) + " = UserNTrans()")
-        exec("u" + str(i) + ".createTransitions(" + nbtrans + ")")
-        exec("ass.addComponent('u" + str(i) + "'," + "u" + str(i) + ")")
-        exec("ass.addConnection(provider, 'service', u" + str(i)
-             + ", 'service')")
-
-    mad = Mad(ass)
-    mad.run()
+        mad = Mad(ass)
+        mad.run()
