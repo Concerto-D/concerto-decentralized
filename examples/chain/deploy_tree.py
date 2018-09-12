034ef62794661fd66b42842befa074ea6b4201ba
Christian Perez <Christian.Perez@inria.fr>
2018-09-12 09:46:07 +0200

    example: feature activated

--- examples/chain/deploy_tree.py
+++ examples/chain/deploy_tree.py
@@ -1,6 +1,18 @@
 import sys
 from mad import *
 
+#
+#
+#  client.u ---- p.n_0_0
+#                    .u
+#                     |
+#        n_1_0.p ----------- p.n_1_1
+#          .u                    .u
+#           |                     |
+#  n_2_0.p --- p.n_2_1   n_2_2.p --- p.n_2_2
+#  ...
+#
+
 from node import Node
 
 def get_name(i,j):
@@ -8,15 +20,21 @@ def get_name(i,j):
 
 if __name__ == '__main__':
 
-    if len(sys.argv) != 2:
+    if len(sys.argv) != 2 and len(sys.argv) != 3:
         print("*** Error: missing parameters!\n")
-        print("deploy.py <high of the tree> (starting to 0)\n")
+        print("deploy.py <high of the tree> (starting to 0) <bug mode (0/1)>\n")
         sys.exit(-1)
     else:
         height = int(sys.argv[1])
         if height < 0:
             print("*** Warning: at least of 1 level. 1 level will be used.\n")
             height = 0
+        if len(sys.argv) == 3:
+            bug = sys.argv[2]
+            if bug == '1':
+                print("Bug mode activated -- connected all nodes to client!")
+        else:
+            bug = '0'
 
         # Composant Provider
         ass = Assembly()
@@ -37,9 +55,14 @@ if __name__ == '__main__':
                 # Connecting the client to all
                 name = get_name(i,j)
                 print("name: "+name)
-                n = nodes[name]                
-                ass.addConnection(n, 'p', client, 'u')
-                print("client connection: "+n.name+'.p <- '+ client.name+'.u')
+                n = nodes[name]
+                if bug == '1':
+                    ass.addConnection(n, 'p', client, 'u')
+                    print("client connection: "+n.name+'.p <- '+ client.name+'.u')
+                else:
+                    if i == 0 and j == 0:
+                        ass.addConnection(n, 'p', client, 'u')
+                        print("client connection: "+n.name+'.p <- '+ client.name+'.u')
                 if i<height-1: # if not last line, connect to 2*j & 2j+1                    
                     n1=nodes[get_name(i+1, 2*j)]
                     n2=nodes[get_name(i+1, 2*j+1)]
