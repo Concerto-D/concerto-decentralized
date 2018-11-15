0213cbcb5b57e724f13df190aad1a672c12151b2
Maverick Chardet <chardetm@gmail.com>
2018-11-15 11:45:19 +0100

    Added step to time scripts

--- examples/scalability/time_deploy_par_up.py
+++ examples/scalability/time_deploy_par_up.py
@@ -5,14 +5,20 @@ import deploy_par_up
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 3:
+    if len(sys.argv) < 3 or len(sys.argv) > 5:
         print("*** Error: missing parameters!\n")
         print("time_deploy_par_up.py <max number of user components> <max number of "
-              "transitions inside user components>\n")
+              "transitions inside user components> (<step nb components> (<step nb transitions>))\n")
         sys.exit(-1)
 
     nb_comp = int(sys.argv[1])
     nb_trans = int(sys.argv[2])
+    step_comp = 1
+    if len(sys.argv) >= 4:
+        step_comp = int(sys.argv[3])
+    step_trans = 1
+    if len(sys.argv) >= 5:
+        step_trans = int(sys.argv[4])
     
     if nb_comp < 1:
         print("*** Warning: at least 1 user component is deployed by "
@@ -28,7 +34,7 @@ if __name__ == '__main__':
 
     writer.writeheader()
     
-    for c in range(1, nb_comp+1):
-        for t in range(1, nb_trans+1):
+    for c in range(1, nb_comp+1, step_comp):
+        for t in range(1, nb_trans+1, step_trans):
             time = deploy_par_up.time_test(c,t)
             writer.writerow({'nb_comp': c, 'nb_trans': t, 'time': time})
--- examples/scalability/time_deploy_seq_up.py
+++ examples/scalability/time_deploy_seq_up.py
@@ -5,12 +5,15 @@ import deploy_seq_up
 
 
 if __name__ == '__main__':
-    if len(sys.argv) != 2:
+    if len(sys.argv) < 2 or len(sys.argv) > 3:
         print("*** Error: missing parameters!\n")
-        print("time_deploy_par_up.py <max number of user components>\n")
+        print("time_deploy_par_up.py <max number of user components> <step>\n")
         sys.exit(-1)
 
     nb_comp = int(sys.argv[1])
+    step = 1
+    if len(sys.argv) >= 3:
+        step = int(sys.argv[2])
     
     if nb_comp < 2:
         print("*** Warning: at least 2 components are deployed by this "
@@ -22,6 +25,6 @@ if __name__ == '__main__':
 
     writer.writeheader()
     
-    for c in range(2, nb_comp+1):
+    for c in range(2, nb_comp+1, step):
         time = deploy_seq_up.time_test(c)
         writer.writerow({'nb_comp': c, 'time': time})
