713957be4c6d18281a647a0bac655ee69d7a56be
Maverick Chardet <chardetm@gmail.com>
2018-11-15 16:15:33 +0100

    Added shorter seq experiment

---
+++ examples/scalability/exp_par_c5_t1_10000.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+from itertools import chain
+from time_deploy_par_up import test_and_print_csv
+
+if __name__ == '__main__':
+    comp_range = [5]
+    trans_range = chain(range(1,20), range(20, 200, 10), range(200, 2000, 50), range(2000, 10001, 100))
+    nb_samples = 5
+    test_and_print_csv(comp_range, trans_range, nb_samples)
+    
---
+++ examples/scalability/exp_seq_2_5000.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+from itertools import chain
+from time_deploy_seq_up import test_and_print_csv
+
+if __name__ == '__main__':
+    exp_range = chain(range(2,20), range(20, 200, 10), range(200, 2000, 100), range(2000, 5001, 500))
+    nb_samples = 5
+    test_and_print_csv(exp_range, nb_samples)
+    
