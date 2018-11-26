03322e4072becbc83d123bf92f28d975a18cceb2
Maverick Chardet <chardetm@gmail.com>
2018-11-26 18:01:38 +0100

    Added Ansible utilities

---
+++ madpp/plugins/ansible.py
@@ -0,0 +1,18 @@
+from subprocess import run, CompletedProcess
+
+class AnsibleCallResult:
+    def __init__(self, command : str, return_code : int, stdout : str, stderr : str):
+        self.command = command
+        self.return_code = return_code
+        self.stdout = stdout
+        self.stderr = stderr
+
+def call_ansible_on_host(host, playbook, tag="all", capture_output=False) -> AnsibleCallResult:
+    command = "ansible-playbook -i %s, %s --tags \"%s\"" % (host,playbook,tag)
+    completed_process = run(command, shell=True, check=False, capture_output=capture_output)
+    return AnsibleCallResult(
+        command = command,
+        return_code = completed_process.returncode,
+        stdout = completed_process.stdout,
+        stderr = completed_process.stderr
+    )
