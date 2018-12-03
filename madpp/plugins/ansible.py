6a590bf18535910e3a9479232af5d41e5e106ca4
Maverick Chardet <chardetm@gmail.com>
2018-12-03 15:20:32 +0100

    Trying one directory per ansible call

--- madpp/plugins/ansible.py
+++ madpp/plugins/ansible.py
@@ -1,5 +1,10 @@
 from subprocess import run, CompletedProcess
 import sys
+import os
+import threading
+
+_ansible_call_lock = threading.Lock()
+_ansible_call_id = 0
 
 class AnsibleCallResult:
     def __init__(self, command : str, return_code : int, stdout : str = None, stderr : str = None):
@@ -9,11 +14,18 @@ class AnsibleCallResult:
         self.stderr = stderr
 
 def call_ansible_on_host(host, playbook, tag="all", extra_vars=None, capture_output=False) -> AnsibleCallResult:
+    global _ansible_call_lock
+    global _ansible_call_id
+    with _ansible_call_lock:
+        directory = "_ansible_calls/%d"%_ansible_call_id
+        _ansible_call_id += 1
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     if extra_vars is None:
         extra_vars_string = ""
     else:
         extra_vars_string = " --extra-vars \"%s\"" % ' '.join(["%s=%s"%(key, str(value)) for key,value in extra_vars.items()])
-    command = "ansible-playbook -i %s, %s --tags \"%s\"%s" % (host,playbook,tag,extra_vars_string)
+    command = "cd %s;ansible-playbook -i %s, %s --tags \"%s\"%s" % (directory,host,playbook,tag,extra_vars_string)
     if sys.version_info >= (3, 7):
         completed_process = run(command, shell=True, check=False, capture_output=capture_output)
         return AnsibleCallResult(
