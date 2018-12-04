8d61e26952cc688f1ed584da7412a8ccde8e963f
Maverick Chardet <chardetm@gmail.com>
2018-12-04 16:10:20 +0100

    DEBUG MARIADB

--- examples/ansible/juice/components/mariadb.py
+++ examples/ansible/juice/components/mariadb.py
@@ -82,8 +82,8 @@ class MariaDB(Component):
     def go_ready(self):
         self.print_color("going ready")
         #time.sleep(5.8)
-        #result = call_ansible_on_host(self.host, self.playbook, "mariadb-5", extra_vars={"enos_action":"deploy", "db":"mariadb", "db_host":self.host})
-        #self.print_color("Database ready (code %d) with command: %s" % (result.return_code, result.command))
+        result = call_ansible_on_host(self.host, self.playbook, "mariadb-5", extra_vars={"enos_action":"deploy", "db":"mariadb", "db_host":self.host})
+        self.print_color("Database ready (code %d) with command: %s" % (result.return_code, result.command))
 
     def change_config(self):
         self.print_color("Changing config")
