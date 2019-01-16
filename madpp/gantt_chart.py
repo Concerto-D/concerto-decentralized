ae609938033c37e08341ab31d3561ce080a777d7
Maverick Chardet <chardetm@gmail.com>
2019-01-16 14:09:42 +0100

    Trying alternative implementation of export_gnuplot

--- madpp/gantt_chart.py
+++ madpp/gantt_chart.py
@@ -35,10 +35,49 @@ class GanttChart():
         
         file.close()
         
+    #def export_gnuplot(self, file_name, title=''):
+        #component_activities = OrderedDict()
+        #tuple_list = []
+        #id = 0
+        
+        #if len(self.log) is 0:
+            #print("Not exporting Gnuplot: empty log.")
+            #return
+            
+        #temp_dict = {}
+        #(_,_,_,_,min_time) = self.log[0]
+        #for (action,component,behavior,transition,time) in self.log:
+            #if component not in component_activities:
+                #component_activities[component] = OrderedDict()
+            #if action is self.START:
+                #if (component,behavior,transition) in temp_dict:
+                    #raise Exception("GanttChart: error during export to gnuplot: action already started")
+                #start_time = time-min_time
+                #temp_dict[(component,behavior,transition)] = id
+                #component_activities[component][id] = (component, behavior, transition, start_time)
+                #id += 1
+            #else:
+                #if (component,behavior,transition) not in temp_dict:
+                    #raise Exception("GanttChart: error during export to gnuplot: action finished before being started")
+                #id_end = temp_dict[(component,behavior,transition)]
+                #end_time = time-min_time
+                #(_, _, _, start_time) = component_activities[component][id_end]
+                #component_activities[component][id_end] = (component, behavior, transition, start_time, end_time)
+                #del temp_dict[(component,behavior,transition)]
+            
+            #for component in component_activities:
+                ##tuple_list.append("+ %s"%component, 0, 0)
+                #for activity in component_activities[component]:
+                    #(_, behavior, transition, start_time, end_time) = component_activities[component][activity]
+                    #name = "%s.%s.%s"%(component,behavior,transition)
+                    #name = name.replace("_","\\\\\\_")
+                    #tuple_list.append((name,start_time,end_time))
+        
+        #gnuplot_file_from_list(tuple_list, file_name, title)
+        
     def export_gnuplot(self, file_name, title=''):
-        component_activities = OrderedDict()
+        component_activities = []
         tuple_list = []
-        id = 0
         
         if len(self.log) is 0:
             print("Not exporting Gnuplot: empty log.")
@@ -47,31 +86,25 @@ class GanttChart():
         temp_dict = {}
         (_,_,_,_,min_time) = self.log[0]
         for (action,component,behavior,transition,time) in self.log:
-            if component not in component_activities:
-                component_activities[component] = OrderedDict()
             if action is self.START:
                 if (component,behavior,transition) in temp_dict:
                     raise Exception("GanttChart: error during export to gnuplot: action already started")
                 start_time = time-min_time
-                temp_dict[(component,behavior,transition)] = id
-                component_activities[component][id] = (component, behavior, transition, start_time)
-                id += 1
+                temp_dict[(component,behavior,transition)] = start_time
             else:
                 if (component,behavior,transition) not in temp_dict:
                     raise Exception("GanttChart: error during export to gnuplot: action finished before being started")
-                id_end = temp_dict[(component,behavior,transition)]
+                start_time = temp_dict[(component,behavior,transition)]
                 end_time = time-min_time
-                (_, _, _, start_time) = component_activities[component][id_end]
-                component_activities[component][id_end] = (component, behavior, transition, start_time, end_time)
+                component_activities.append(component, behavior, start_time, end_time, transition)
                 del temp_dict[(component,behavior,transition)]
+                
+            component_activities.sort()
             
-            for component in component_activities:
-                #tuple_list.append("+ %s"%component, 0, 0)
-                for activity in component_activities[component]:
-                    (_, behavior, transition, start_time, end_time) = component_activities[component][activity]
-                    name = "%s.%s.%s"%(component,behavior,transition)
-                    name = name.replace("_","\\\\\\_")
-                    tuple_list.append((name,start_time,end_time))
+            for (component, behavior, start_time, end_time, transition) in component_activities:
+                name = "%s.%s.%s"%(component,behavior,transition)
+                name = name.replace("_","\\\\\\_")
+                tuple_list.append((name,start_time,end_time))
         
         gnuplot_file_from_list(tuple_list, file_name, title)
         
