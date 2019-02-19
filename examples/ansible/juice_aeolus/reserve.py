4a139af9fb92e7739b10bb70bf388ad4350891d1
Maverick Chardet <chardetm@gmail.com>
2019-02-19 17:34:04 +0100

    Added a few attempts

--- examples/ansible/juice_aeolus/reserve.py
+++ examples/ansible/juice_aeolus/reserve.py
@@ -210,7 +210,7 @@ def experiments():
         sweeps=sweep({
             'nb_db_nodes': [3, 5, 10], #, 20],
             'nb_db_entries': [1000], #, 0, 10000, 100000],
-            'attempt': [1,2,3,4,5,6,7,8,9,10]
+            'attempt': [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15]
         }))
     
     logging.info("Using Execo sweeper in directory: %s"%SWEEPER_DIR)
