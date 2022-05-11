2d842dca6e0da9ae62a0a2478ee82ba247cf3e75
aomond <antoine.omond@irisa.fr>
2022-05-11 11:01:18 +0200

    Update output status_routers.sh

--- zenoh-router/status_routers.sh
+++ zenoh-router/status_routers.sh
@@ -3,15 +3,16 @@
 # On récupère le nombre de containers runnings
 ND=$(sudo docker ps -q)
 N=$(echo $ND | grep -o " " | wc -l)
-N=$(($N+1))
+N=$((N+1))
 
-# On boucle sur les ports de 8000 à 8000+$N
-RESULT_TO_WATCH=""
-for ((i = 0 ; i < $N ; i++)); do
-  PR=$((8000+$i))
-  ROUTER_PID=$(curl localhost:$PR/@/router/local | jq -r '.[0].value.pid')
-  echo "ROUTER_PID: $ROUTER_PID" # Affiche le PID du routeur
-  echo "HTTP_PORT: $PR"          # Affiche le port HTTP sur lequel l'API REST est accessible
-  curl localhost:$PR/**          # Affiche les données insérées dans le router ayant pour clé /**
+# On boucle sur les ports de 8000 à 8000+N
+for ((i = 0 ; i < N ; i++)); do
+  PR=$((8000+i))
+  ROUTER_PID=$(curl --no-progress-meter localhost:$PR/@/router/local | jq -r '.[0].value.pid')
+  echo "ROUTER_PID: $ROUTER_PID"             # Affiche le PID du routeur
+  echo "HTTP_PORT: $PR"                      # Affiche le port HTTP sur lequel l'API REST est accessible
+  curl --no-progress-meter localhost:$PR/**  # Affiche les données insérées dans le router ayant pour clé /**
+  echo ""
+  echo ""
 done
 
