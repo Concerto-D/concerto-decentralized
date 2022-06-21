bff187f1b0c59c494671b48f14c08d65c60ccb71
aomond-imt <antoine.omond@irisa.fr>
2022-06-21 16:19:05 +0200

    #6 - experience - Added timestamp to dir_to_save name

--- evaluation/experiment/script_launcher.py
+++ evaluation/experiment/script_launcher.py
@@ -219,7 +219,8 @@ def launch_experiment(uptimes_params_nodes, transitions_times, cluster):
         json.dump(results, f, indent=4)
 
     # Save config expe + results
-    dir_to_save_path = f"/home/anomond/all_expe_results/{reconfig_config_file_path}"
+    datetime_now_formatted = datetime.now().strftime("%Y-%m-%d_%H-%M-%S")
+    dir_to_save_path = f"/home/anomond/all_expe_results/{datetime_now_formatted}_{reconfig_config_file_path}"
     os.makedirs(dir_to_save_path)
 
     # Save uptimes
